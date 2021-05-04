2eeb03cb02f13f88941b2a0aae6c9d50982d509f
Taka2401 <tkht2401@gmail.com>
2021-05-04 04:02:39 +0000

    [add] header&footer

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'homes#top'
-  get 'homes/about'
+  get '/about' => 'homes#about'
 
   resources :camps, only: [:index, :show]
   resources :users, only: [:show, :edit, :update]
