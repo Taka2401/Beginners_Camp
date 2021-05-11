2223d3ddd7f1023d7a91533383944cf9fa8c1f65
Taka2401 <tkht2401@gmail.com>
2021-05-11 05:59:52 +0000

    画面スライド追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,27 +28,16 @@ $(document).ready(function () {
     navType : 'block',
     // 子要素の種類('div' or 'img')
     childrenElementType : 'div',
-    // ナビゲーション矢印の表示(trueで表示)
-    arrows : true,
     // スライドショーの自動再生(falseで自動再生なし)
     autoPlay : true,
     // 自動再生時のスライド切替間隔(ミリ秒)
     autoPlayDuration : 3000,
-    // キーボードの矢印キーによるスライド送りの設定(trueで有効)
-    keyboardOnAlways : true,
     // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
     hidePrevious : false
   });
 });
 
-$(function() {
-    $('.slide-image').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-    });
-});
+
 
 
 
