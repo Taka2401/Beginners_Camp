a0ecb077d7a98f94db5bb8f94c548479b3eb6b8d
Taka2401 <tkht2401@gmail.com>
2021-05-30 04:09:10 +0000

    deploy setting

--- app/controllers/public/camp_item_values_controller.rb
+++ app/controllers/public/camp_item_values_controller.rb
@@ -19,6 +19,7 @@ class Public::CampItemValuesController < ApplicationController
     @camp_item = CampItem.find(params[:camp_item_id])
     @camp_item_value = CampItemValue.new(camp_item_value_params)
     @camp_item_value.camp_item_id = @camp_item.id
+    @camp_item_value.user_id = current_user.id
     if @camp_item_value.save
       flash[:notice] = "口コミを投稿しました！"
     redirect_to camp_item_path(@camp_item)
