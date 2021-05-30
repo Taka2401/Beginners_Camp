549566cbd8d3f1c685b54859cce9233fee76da03
Taka2401 <tkht2401@gmail.com>
2021-05-30 03:11:09 +0000

    depoly setting

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,4 @@
 class Tag < ApplicationRecord
-  has_many :tag_relationships, dependent: :destroy, foreign_key: 'tag_id'
+  has_many :tag_relationships, dependent: :destroy
   has_many :posts, through: :tag_relationships
 end
