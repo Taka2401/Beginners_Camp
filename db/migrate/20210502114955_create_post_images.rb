aaaecde92703e6113434d3a207362a14834c4e9b
Taka2401 <tkht2401@gmail.com>
2021-06-12 04:49:04 +0000

    テスト

--- db/migrate/20210502114955_create_post_images.rb
+++ db/migrate/20210502114955_create_post_images.rb
@@ -1,7 +1,6 @@
 class CreatePostImages < ActiveRecord::Migration[5.2]
   def change
     create_table :post_images do |t|
-      t.integer :user_id
       t.integer :post_id
       t.integer :camp_place_id
 
--- db/migrate/20210608063323_remove_user_id_and_post_id_and_camp_item_id_from_post_images.rb
+++ db/migrate/20210608063323_remove_user_id_and_post_id_and_camp_item_id_from_post_images.rb
@@ -1,6 +1,5 @@
 class RemoveUserIdAndPostIdAndCampItemIdFromPostImages < ActiveRecord::Migration[5.2]
   def change
-    remove_column :post_images, :user_id, :inteder
     remove_column :post_images, :post_id, :integer
     remove_column :post_images, :camp_item_id, :integer
   end
