10d7b347c6eac4ab27811baee8f681b2d01b7f70
Taka2401 <tkht2401@gmail.com>
2021-08-12 04:29:20 +0000

    fix: pagination

--- app/controllers/public/camp_items_controller.rb
+++ app/controllers/public/camp_items_controller.rb
@@ -1,6 +1,6 @@
 class Public::CampItemsController < ApplicationController
   def index
-    @camp_items = CampItem.all.page(params[:page]).per(10)
+    @camp_items = CampItem.all.page(params[:page]).per(8)
   end
 
   def show
