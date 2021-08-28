0069ea213cfa6f2702b94e7d21d5ee2e1acb9c59
Taka2401 <tkht2401@gmail.com>
2021-08-28 13:36:53 +0900

    fix: docker-composeエラー修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
-  # devise_for :admins,
-  #            controllers: {
-  #              sessions: 'admins/sessions',
-  #              passwords: 'admins/passwords',
-  #              registrations: 'admins/registrations',
-  #            }
-  # devise_for :users,
-  #            controllers: {
-  #              sessions: 'users/sessions',
-  #              passwords: 'users/passwords',
-  #              registrations: 'users/registrations',
-  #            }
+  devise_for :admins,
+             controllers: {
+               sessions: 'admins/sessions',
+               passwords: 'admins/passwords',
+               registrations: 'admins/registrations',
+             }
+  devise_for :users,
+             controllers: {
+               sessions: 'users/sessions',
+               passwords: 'users/passwords',
+               registrations: 'users/registrations',
+             }
 
-  # devise_scope :user do
-  #   post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
-  # end
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
 
   # ========= ユーザー(public)のルーティング ================
   scope module: :public do
