99754024f7f1f24df2b82374037d47679e9b1f71
Taka2401 <tkht2401@gmail.com>
2021-08-28 04:54:27 +0000

    fix: routing amdins

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :admin,
+  devise_for :admins,
              controllers: {
                sessions: 'admins/sessions',
                passwords: 'admins/passwords',
