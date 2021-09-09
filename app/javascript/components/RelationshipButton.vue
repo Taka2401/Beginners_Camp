7cbb9ef473074012742f3d9b839a56a89e07b0d5
Taka2401 <tkht2401@gmail.com>
2021-09-09 11:30:17 +0900

    fix: ファイル名、ルーティング修正

---
+++ app/javascript/components/RelationshipButton.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <div v-if="isRelationshiped" @click="deleteRelationship()" class="btn btn-bg follow-followed">
+      <i class="fas fa-user-minus"></i> フォロー解除
+    </div>
+    <div v-else @click="registerRelationship()" class="btn btn-bg" style="margin:0;">
+      <i class="fas fa-user-plus"></i> フォローする
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import { csrfToken } from 'rails-ujs'
+axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken()
+
+export default {
+  props: ['followerId', 'followedId'],
+  data() {
+    return {
+      relationshipList: []
+    }
+  },
+  computed: {
+    count() {
+      return this.relationshipList.length
+    },
+    isRelationshiped() {
+      if (this.relationshipList.length === 0) { return false }
+      return Boolean(this.findRelationshipId())
+    }
+  },
+  created: function() {
+    this.fetchRelationshipByFollowedId().then(result => {
+      this.relationshipList = result
+    })
+  },
+  methods: {
+    fetchRelationshipByFollowedId: async function() {
+      const res = await axios.get(`/api/relationships/?followed_id=${this.followedId}`)
+      if (res.status !== 200) { process.exit() }
+      return res.data
+    },
+    registerRelationship: async function() {
+      const res = await axios.post('/api/relationships', { followed_id: this.followedId })
+      if (res.status !== 201) { process.exit() }
+      this.fetchRelationshipByFollowedId().then(result => {
+        this.relationshipList = result
+      })
+    },
+    deleteRelationship: async function() {
+      const relationshipId = this.findRelationshipId()
+      const res = await axios.delete(`/api/relationships/${relationshipId}`)
+      if (res.status !== 200) { process.exit() }
+      this.relationshipList = this.relationshipList.filter(n => n.id !== relationshipId)
+    },
+    findRelationshipId: function() {
+      const relationship = this.relationshipList.find((relationship) => {
+        return (relationship.follower_id === this.followerId)
+      })
+      if (relationship) { return relationship.id }
+    }
+  }
+}
+</script>
--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1, format: 'json' do
-      resources :relationships, only: [create, destroy]
+      resources :relationships, only: [:create, :destroy]
     end
   end
 
