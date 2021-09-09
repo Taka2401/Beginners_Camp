b1094507e90d0353dcfe1f3197d808834ddf1a2f
Taka2401 <tkht2401@gmail.com>
2021-09-09 09:32:15 +0900

    view画面作成

---
+++ app/controllers/api/v1/relationships.controller.rb
@@ -0,0 +1,25 @@
+module Api
+  module v1
+    class RelationshipsController < ActionController::API
+      before_action :authenticate_user!
+
+      def create
+        @user = User.find(params[:followed_id])
+        current_user.active_relationships.create!(relationships_params)
+        @user.create_notification_follow!(current_user)
+        head :created
+      end
+
+      def destroy
+        current_user.active_relationships.find(params[:id]).destroy!
+        head :ok
+      end
+
+      private
+
+      def relationships_params
+        params.require(:relationship).permit(:followed_id)
+      end
+    end
+  end
+end
