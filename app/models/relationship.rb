1edf998a0e6b061d2a172a9a091841bc619693ac
Taka2401 <tkht2401@gmail.com>
2021-09-11 21:44:11 +0900

    relationships バリデーション追加

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,5 +2,8 @@ class Relationship < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
 
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
+
   scope :filter_by_followed, ->(followed_id) { where(followed_id: followed_id) if followed_id }
 end
