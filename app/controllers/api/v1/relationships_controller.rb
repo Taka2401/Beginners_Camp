576f5eb45d2333c153f7f54dd3f04416fbb6968c
Taka2401 <tkht2401@gmail.com>
2021-09-12 13:34:51 +0900

    test: フォローボタン

--- app/controllers/api/v1/relationships_controller.rb
+++ app/controllers/api/v1/relationships_controller.rb
@@ -3,20 +3,24 @@ module Api
     class RelationshipsController < ActionController::API
       before_action :authenticate_user!
 
+      def index
+        render json: Relationship.filter_by_followed(params[:followed_id]).select(:id, :follower_id, :followed_id)
+      end
+
       def create
-        current_user.follow(relationships_params)
+        current_user.relationships.create(relationships_params)
         head :created
       end
 
       def destroy
-        current_user.unfollow(params[:user_id])
+        current_user.relationships.find(params[:id]).destroy!
         head :ok
       end
 
       private
 
       def relationships_params
-        params.require(:relationship).permit(:user_id, :followed_id)
+        params.require(:relationship).permit(:followed_id)
       end
     end
   end
