1bcae6aafaa72ad136f000798eb4b52a15725d06
Taka2401 <tkht2401@gmail.com>
2021-10-13 14:37:31 +0900

    style: Rubcopで静的コード解析

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -5,5 +5,4 @@ class Public::BookmarksController < ApplicationController
     @user = User.find(params[:user_id])
     @bookmarks = CampPlace.where(id: @user.bookmarks.pluck(:Camp_place_id)).page(params[:page]).per(8).order(id: "DESC")
   end
-
 end
